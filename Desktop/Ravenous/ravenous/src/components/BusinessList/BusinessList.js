ee677966b3549359b387acdbf56f32a80ac63f71
Derek <derekf542@hotmail.com>
2017-12-14 12:20:13 -0700

    Added Ravenous

---
+++ Desktop/Ravenous/ravenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+    render() {
+        return (
+            <div className="BusinessList">
+              <Name /> 
+              <Address />
+              <City />
+              <State />
+              <Zipcode />
+              <Category />
+            </div>
+        );
+    }
+}
+
+export default BusinessList;
