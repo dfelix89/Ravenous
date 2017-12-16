abfe420c09b13a864693e36717fd085c59add35e
Derek <derekf542@hotmail.com>
2017-12-16 11:41:27 -0700

    Fixed problems causing errors

--- Desktop/Ravenous/ravenous/src/App.js
+++ Desktop/Ravenous/ravenous/src/App.js
@@ -10,30 +10,9 @@ class App extends React.Component {
     return (
      <div className="App">
       <h1>ravenous</h1>
-      <div className="SearchBar">
-              <div className="SearchBar-sort-options">
-                <ul>
-                  .renderSortByOptions()
-                </ul>
-              </div>
-              <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
-              </div>
-              <div className="SearchBar-submit">
-                <a>Let's Go</a>
-              </div>
-            </div>
-      <div className="BusinessList">
-              <h2> {this.props.business.name} </h2> 
-              <h3> {this.props.business.address} </h3>
-              <h3> {this.props.business.city} </h3>
-              <h3> {this.props.business.state} </h3>
-              <h3> {this.props.business.zipCode} </h3>
-              <h3> {this.props.business.category} </h3>
-            </div> 
+      <SearchBar />
+      <BusinessList />
      </div>
-     
     );
   }
 }
--- Desktop/Ravenous/ravenous/src/components/BusinessList/BusinessList.js
+++ Desktop/Ravenous/ravenous/src/components/BusinessList/BusinessList.js
@@ -3,18 +3,18 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
-    render() {
-        return (
-            <div className="BusinessList">
-              <h2> {this.props.business.name} </h2>
-              <h3> {this.props.business.address} </h3>
-              <h3> {this.props.business.city} </h3>
-              <h3> {this.props.business.state} </h3>
-              <h3> {this.props.business.zipCode} </h3>
-              <h3> {this.props.business.category} </h3>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
 }
 
 export default BusinessList;
